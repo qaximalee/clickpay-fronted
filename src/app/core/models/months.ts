37d71ef0a644286dcdd9b6ddddb353ffd2b56b62
Muhammad Tahir <tahir@vinncorp.com>
2023-02-06 12:34:10 +0500

    Add enums.

---
+++ src/app/core/models/months.ts
@@ -0,0 +1,14 @@
+export enum PaymentType {
+    January = 'JAN',
+    February = 'FEB',
+    March = 'MARCH',
+    April = 'APRIL',
+    May = 'MAY',
+    June = 'JUNE',
+    July = 'JULY',
+    August = 'AUG',
+    September = 'SEPT',
+    October = 'OCT',
+    November = 'NOV',
+    December = 'DEC',
+}
